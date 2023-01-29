b2289f22a73ea6b26137cda71f403ed1de0003bc
MGspring <wlsaudrhkd1@gmail.com>
2023-01-30 00:32:08 +0900

    2023/01/29 ~2chapter

--- src/test/java/com/example/tacos/HomeControllerTest.java
+++ src/test/java/com/example/tacos/HomeControllerTest.java
@@ -10,9 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
-import com.example.tacos.controller.HomeController;
-
-@WebMvcTest(HomeController.class)
+@WebMvcTest
 public class HomeControllerTest {
 
 	@Autowired
